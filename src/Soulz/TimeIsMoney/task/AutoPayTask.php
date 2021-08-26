73d75a3c201dc271e43e0e3fd226b9a36b191608
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-08-25 20:34:37 -0700

    pray on it

---
+++ src/Soulz/TimeIsMoney/task/AutoPayTask.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Soulz\TimeIsMoney\task;
+
+use pocketmine\scheduler\Task;
+use pocketmine\utils\Config;
+use Soulz\TimeIsMoney\Loader;
+use onebone\economyapi\EconomyAPI;
+
+class AutoPayTask extends Task {
+
+    /** @var Loader */
+    protected $loader;
+
+    /**
+     * @param Loader $loader
+     */
+    public function __construct(Loader $loader) {
+        $this->loader = $loader;
+    }
+
+    /**
+     * @param int $currentTick
+     */
+    public function onRun(int $currentTick) {
+        foreach($this->loader->getServer()->getOnlinePlayers() as $player){
+            if($this->loader->getPlayer($player) !== null){
+                # This is different from what I usually do, so hopefully it works
+                $this->loader->getPlayer($player) = EconomyAPI::getInstance()->addMoney($player, $this->loader->getConfig()->get("money-gain"));
+            }
+        }
+    }
+}
+}
