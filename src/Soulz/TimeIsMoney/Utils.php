4863206065913a29f027336b016826b75dbf3d59
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-08-26 06:53:07 -0700

    Stores Incline and Decline

---
+++ src/Soulz/TimeIsMoney/Utils.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Soulz\TimeIsMoney;
+
+class Utils {
+
+    public const DECLINE = TextFormat::GRAY . "[" . TextFormat::BOLD . TextFormat::RED . "!" . TextFormat::RESET . TextFormat::GRAY . "]" . TextFormat::RESET;
+
+    public const INCLINE = TextFormat::GRAY . "[" . TextFormat::BOLD . TextFormat::GREEN . "!" . TextFormat::RESET . TextFormat::GRAY . "]" . TextFormat::RESET;
+}
