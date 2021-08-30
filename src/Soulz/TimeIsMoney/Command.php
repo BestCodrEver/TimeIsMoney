9f618e150beabe4aca235cc7f669e8e5a9b6c8aa
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-08-30 13:45:11 -0700

    Implements PluginIdentifiableCommand
    PluginIdentifiableCommand added for Poggit

--- src/Soulz/TimeIsMoney/Command.php
+++ src/Soulz/TimeIsMoney/Command.php
@@ -4,18 +4,19 @@ namespace Soulz\TimeIsMoney;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
 use pocketmine\utils\TextFormat;
 use Soulz\TimeIsMoney\Loader;
 
-class Command extends Commmand {
+class Command extends Commmand implements PluginIdentifiableCommand {
 
     public function __construct(){
-        parent::__construct("timeismoney", "View Time Is Money", "/timeismoney [info, auto]", null);
+        parent::__construct("timeismoney", "View Time Is Money", "/timeismoney [info, auto]", ['timemoney']);
         $this->setPermission("time.is.money.command");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-        if(!isset(args[0])) return $sender->sendMessage(TextFormat::RED . "Insufficient Args!" . TextFormat::GRAY . "/timeismoney [info, auto]");
+        if(!isset(args[0])) return $sender->sendMessage(TextFormat::RED . "Insufficient Args! " . TextFormat::GRAY . "/timeismoney [info, auto]");
         if(!$sender->hasPermission("time.is.money.command")){
             $sender->sendMessage(TextFormat::GRAY . "You do not have permission to execute this command!");
         }
@@ -24,12 +25,15 @@ class Command extends Commmand {
         }
         switch(args[0]){
             case "info":
-                $sender->sendMessage(TextFormat::GOLD . "Time is Money " . TextFormat::GRAY . "allows players to automatically gain money from simple task!\n" . TextFormat::GRAY . "Execute /timeismoney [auto] to view how money is earned!");
+                $sender->sendMessage(TextFormat::GOLD . "Time is Money " . TextFormat::GRAY . "allows players to automatically gain money from simple tasks!\n" . TextFormat::GRAY . "Execute /timeismoney [auto] to view how money is earned!");
                 break;
             case "auto":
-                $sender->sendMessage(TextFormat::GRAY . "Money is earned by staying online, mining, building and killing players! You can modify amounts earned via" . TextFormat::GOLD . "config" . TextFormat::GRAY . "!");
+                $sender->sendMessage(TextFormat::GRAY . "Money is earned by staying online, mining, building and killing players! You can modify amounts earned via " . TextFormat::GOLD . "config" . TextFormat::GRAY . "!");
                 break;
         }
         
     }
+
+    public function getPlugin(): Plugin {
+		return Loader::getInstance();
 }
